267d457668c0c2e42799c4266ad2dd7f86413c67
Stefan Theofilos <StefanTheofilos@Stefans-MacBook-Pro.local>
2016-04-08 09:16:21 -0500

    Almost done

--- app/routes/bookmarks.js
+++ app/routes/bookmarks.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return fetch(`https://tiny-tn.herokuapp.com/collections/bookmarks`)
-    .then((res) => res.json());
+    return fetch(`https://tiny-tn.herokuapp.com/collections/st-bookmarks`)
+    .then(res => res.json());
   }
 });
